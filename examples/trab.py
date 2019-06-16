8f863cd4fa3375fcc561e6426838555a9941fd6f
JukiOK <oky1773@gmail.com>
2019-06-15 23:43:02 -0300

    Adicionado imagens

--- examples/trab.py
+++ examples/trab.py
@@ -42,51 +42,43 @@ def braitenberg(dist, vel):
     return [vLeft, vRight]
 
 def wall_follow(dist):
-    distance_left = min(dist[:3])
-    distance_right = min(dist[5:])
-    distance_front = min(dist[3:5])
+    distance_left = min(dist[:4])
+    distance_right = min(dist[4:])
     walking.input['distance left'] = distance_left
     walking.input['distance right'] = distance_right
-    walking.input['distance front'] = distance_front
     walking.compute()
 
     return [walking.output['velocity left'], walking.output['velocity right']]
 
-distance_left = ctrl.Antecedent(np.arange(0.3, 5.01, 0.01), 'distance left')
-distance_right = ctrl.Antecedent(np.arange(0.3, 5.01, 0.01), 'distance right')
-distance_front = ctrl.Antecedent(np.arange(0.3, 5.01, 0.01), 'distance front')
+distance_left = ctrl.Antecedent(np.arange(0.2, 5.01, 0.01), 'distance left')
+distance_right = ctrl.Antecedent(np.arange(0.2, 5.01, 0.01), 'distance right')
 
 velocity_left = ctrl.Consequent(np.arange(0.0,5.1,0.1), 'velocity left')
 velocity_right = ctrl.Consequent(np.arange(0.0,5.1,0.1), 'velocity right')
 
-distance_left['low'] = fuzz.trimf(distance_left.universe, [0.3, 0.3, 0.5])
-distance_left['medium'] = fuzz.trimf(distance_left.universe, [0.4, 0.5, 1.0])
-distance_left['high'] = fuzz.trapmf(distance_left.universe, [0.6, 1.0, 5.0, 5.0])
+distance_left['low'] = fuzz.trimf(distance_left.universe, [0.2, 0.2, 0.3])
+distance_left['medium'] = fuzz.trimf(distance_left.universe, [0.2, 0.3, 1.0])
+distance_left['high'] = fuzz.trapmf(distance_left.universe, [0.5, 1.0, 5.0, 5.0])
 
-distance_right['low'] = fuzz.trimf(distance_right.universe, [0.3, 0.3, 0.5])
-distance_right['medium'] = fuzz.trimf(distance_right.universe, [0.4, 0.5, 1.0])
-distance_right['high'] = fuzz.trapmf(distance_right.universe, [0.6, 1.0, 5.0, 5.0])
+distance_right['low'] = fuzz.trimf(distance_right.universe, [0.2, 0.2, 0.3])
+distance_right['medium'] = fuzz.trimf(distance_right.universe, [0.2, 0.3, 1.0])
+distance_right['high'] = fuzz.trapmf(distance_right.universe, [0.5, 1.0, 5.0, 5.0])
 
-distance_front['low'] = fuzz.trimf(distance_front.universe, [0.3, 0.3, 0.5])
-distance_front['medium'] = fuzz.trimf(distance_front.universe, [0.4, 0.5, 1.0])
-distance_front['high'] = fuzz.trapmf(distance_front.universe, [0.6, 1.0, 5.0, 5.0])
+velocity_left['low'] = fuzz.trimf(velocity_left.universe, [0, 0, 3.0])
+velocity_left['medium'] = fuzz.trimf(velocity_left.universe, [0, 3.0, 5.0])
+velocity_left['high'] = fuzz.trimf(velocity_left.universe, [3.0, 5.0, 5.0])
 
-velocity_left['low'] = fuzz.trimf(velocity_left.universe, [0, 0, 2.5])
-velocity_left['medium'] = fuzz.trimf(velocity_left.universe, [0, 2.5, 5.0])
-velocity_left['high'] = fuzz.trimf(velocity_left.universe, [2.5, 5.0, 5.0])
+velocity_right['low'] = fuzz.trimf(velocity_right.universe, [0, 0, 3.0])
+velocity_right['medium'] = fuzz.trimf(velocity_right.universe, [0, 3.0, 5.0])
+velocity_right['high'] = fuzz.trimf(velocity_right.universe, [3.0, 5.0, 5.0])
 
-velocity_right['low'] = fuzz.trimf(velocity_right.universe, [0, 0, 2.5])
-velocity_right['medium'] = fuzz.trimf(velocity_right.universe, [0, 2.5, 5.0])
-velocity_right['high'] = fuzz.trimf(velocity_right.universe, [2.5, 5.0, 5.0])
-
-rule0 = ctrl.Rule(distance_left['high'] & distance_right['high'] & distance_front['high'], consequent = (velocity_left['high'],velocity_right['high']))
-rule1 = ctrl.Rule((distance_left['low'] & (distance_right['medium'] | distance_right['high'])), consequent = (velocity_right['low'] , velocity_left['high']))
+rule0 = ctrl.Rule(distance_left['high'] & distance_right['high'], consequent = (velocity_left['high'],velocity_right['high']))
+rule1 = ctrl.Rule((distance_left['low']), consequent = (velocity_right['low'] , velocity_left['high']))
 rule2 = ctrl.Rule((distance_right['low'] & (distance_left['medium'] | distance_left['high'])), consequent = (velocity_right['high'] , velocity_left['low']))
-rule3 = ctrl.Rule((distance_front['medium']), consequent = (velocity_right['low'], velocity_left['medium']))
-rule4 = ctrl.Rule(distance_front['low'], consequent = (velocity_right['low'], velocity_left['high']))
-rule5 = ctrl.Rule((distance_left['medium'] & (distance_right['medium'] | distance_right['high'])), consequent = (velocity_right['medium'], velocity_left['low']))
+rule3 = ctrl.Rule((distance_left['medium'] & distance_right['medium']), consequent = (velocity_right['low'], velocity_left['medium']))
+rule4 = ctrl.Rule((distance_left['medium'] & (distance_right['low'] | distance_right['high'])), consequent = (velocity_right['medium'],velocity_left['low']))
 
-velocity_control = ctrl.ControlSystem(rules=[rule0, rule1, rule2, rule3, rule4, rule5])
+velocity_control = ctrl.ControlSystem(rules=[rule0, rule1, rule2, rule3, rule4])
 
 walking = ctrl.ControlSystemSimulation(velocity_control)
 
